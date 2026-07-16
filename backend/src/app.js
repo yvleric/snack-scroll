15ca789b53a069313f6afb1dca944e869504b224
ericsinghh <singheric890@gmail.com>
2026-07-17 00:54:47 +0530

    structure and rebuild MongoDB driver dependencies

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,13 @@
 // create server 
 const express = require('express');
-
+const cookieParser = require('cookie-parser');
 
 const app = express();
+app.use(cookieParser());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send()
+    res.send("Hello World");
 })
 
 
--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,43 @@
-const userModel = require("../models/user.model")
+const userModel = require("../models/user.model");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 async function registerUser(req, res) {
 
     const { fullName, email, password } = req.body;
+
+    const isUserAlreadyExists = await userModel.findOne({
+        email
+    })
+
+    if (isUserAlreadyExists) {
+        return res.status(400).json({
+            message: "User already exists"
+        })
+    }
+    // user not exists 
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    const user = await userModel.create({
+        fullName,
+        email,
+        password: hashPassword
+    })
+
+    const token = jwt.sign({
+        id: user._id,
+    }, "70e3bc1c9560ad7a17cc5731b58ca1056530454e")
+
+    res.cookie("token", token)
+
+    res.status(201).json({
+        message: "User registered successfully",
+        user: {
+            _id: user._id,
+            email: user.email,
+            fullName: user.fullName
+        }
+    })
+
 }
+
